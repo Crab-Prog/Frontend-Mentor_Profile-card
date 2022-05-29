afdab2405559559aa110c89da75ae797e61954e8
Crab-Prog <ophelie.coudert@gmail.com>
2022-05-29 23:58:06 +0200

    Project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='big-container'>
+        <header>
+        </header>
+
+        <main>
+          <div className='profile-title'>
+            <h1>Victor Crest <em>26</em> </h1>
+
+            <p className='city'>London</p>
+          </div>
+          <div className='social-numbers'>
+            <div className='numbers'>
+              <p>80K</p>
+              <em>Followers</em>
+            </div>
+            <div className='numbers'>
+              <p>803K</p>
+              <em>Likes</em>
+            </div>
+            <div className='numbers'>
+              <p>1.4K</p>
+              <em>Photos</em>
+            </div>
+          </div>
+        </main>
+
+        <footer>
+          <div class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
+            Coded by <a href="https://www.frontendmentor.io/profile/Crab-Prog">Ophélie Coudert</a>.
+          </div>
+        </footer>
+
+      </div>
     </div>
   );
 }
